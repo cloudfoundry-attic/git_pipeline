7e93237fad4e8141421acbfd86cba51a4ad88f19
Dmitriy Kalinin <cppforlife@gmail.com>
2013-09-17 20:27:09 -0700

    rails new

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem "rails", "4.0.0"
+gem "haml"
+gem "git"
+
+# CF support
+gem "uglifier"
+gem "rails_12factor", group: :production
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GitPipeline::Application.config.session_store :cookie_store, key: '_git_pipeline_session'
